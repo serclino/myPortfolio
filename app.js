2dbaf924062652129e4ef917f140ae9eff5d0316
serclino <sercl.adam@email.cz>
2021-07-30 11:04:35 +0200

    portrait mobile done, no animations yet

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const arrow = document.querySelector('.arrow');
 
 const tl = new TimelineMax();
 
-tl.fromTo(hero,1, {height: '0%'}, {height: '80%', ease: Power2.easeInOut})
-.fromTo(hero,1.2, {width: '100%'}, {width: '70%', ease: Power2.easeInOut})
-.fromTo(slider,1.2, {x: '200%'}, {x: '0%', ease: Power2.easeInOut}, "-=1.2")
-.fromTo(nav,1, {x: '-110%'}, {x: '0%', ease: Power2.easeInOut})
-.fromTo(headline,1, {opacity: '0', x: '-30'}, {opacity: '1', x: '0', ease: Power2.easeInOut}, "-=1")
-.fromTo(arrow,0.5, {opacity: '0'}, {opacity: '1', ease: Power2.easeInOut}, "+=1.3")
 
 
+
+if (window.innerWidth > 600) {
+    tl.fromTo(hero,1, {height: '0%'}, {height: '80%', ease: Power2.easeInOut})
+    .fromTo(hero,1.2, {width: '100%'}, {width: '70%', ease: Power2.easeInOut})
+    .fromTo(slider,1.2, {x: '200%'}, {x: '0%', ease: Power2.easeInOut}, "-=1.2")
+    .fromTo(nav,1, {x: '-110%'}, {x: '0%', ease: Power2.easeInOut})
+    .fromTo(headline,1, {opacity: '0', x: '-30'}, {opacity: '1', x: '0', ease: Power2.easeInOut}, "-=1")
+    .fromTo(arrow,0.5, {opacity: '0'}, {opacity: '1', ease: Power2.easeInOut}, "+=1.3")
+}
