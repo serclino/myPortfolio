0d06fab02a818bbdfc314610fb6dc241fc0d93aa
serclino <sercl.adam@email.cz>
2021-07-30 10:39:50 +0200

    header - arrow

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const hero = document.querySelector('.hero');
 const slider = document.querySelector('.slider');
 const headline = document.querySelector('.headline');
 const nav = document.querySelector('nav');
+const arrow = document.querySelector('.arrow');
 
 const tl = new TimelineMax();
 
@@ -10,3 +11,6 @@ tl.fromTo(hero,1, {height: '0%'}, {height: '80%', ease: Power2.easeInOut})
 .fromTo(slider,1.2, {x: '200%'}, {x: '0%', ease: Power2.easeInOut}, "-=1.2")
 .fromTo(nav,1, {x: '-110%'}, {x: '0%', ease: Power2.easeInOut})
 .fromTo(headline,1, {opacity: '0', x: '-30'}, {opacity: '1', x: '0', ease: Power2.easeInOut}, "-=1")
+.fromTo(arrow,0.5, {opacity: '0'}, {opacity: '1', ease: Power2.easeInOut}, "+=1.3")
+
+
